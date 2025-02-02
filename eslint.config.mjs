b259d06cbe86ee0b8843bc72cc3a936882fb7a23
root <srotavio.p.freire@outlook.com>
2025-02-02 19:20:55 +0000

    test husky

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,28 @@
-import pluginJs from "@eslint/js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-export default [
-    pluginJs.configs.recommended,
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-    {
-        rules: {
-            "no-loops/no-loops": 2
-        }
-    }
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  // Estendendo as configurações do Next.js com TypeScript
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Adicionando regras customizadas
+  {
+    rules: {
+      // Adiciona a regra para restringir o uso de loops 'for'
+      "no-restricted-syntax": [
+        "error",
+        "ForStatement", // Proíbe o uso de loops 'for'
+      ],
+    },
+  },
 ];
+
+export default eslintConfig;
